9860c916be326f3e539a06d718dcb2b844293c2a
Chishin <pro@xxgzs.org>
2018-09-11 13:37:52 +0800

    增加丰胸仪波形测试工装项目

---
+++ 丰胸仪波形测试工装/frmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 丰胸仪波形测试工装
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
